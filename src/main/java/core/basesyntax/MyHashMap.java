cb7c8529837656860532088d701e7ed87ad52525
Vitalii Levda <vitalii.levda@tul.cz>
2025-09-18 21:59:54 +0300

    getIndex(), resize(), basic fields added

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -1,10 +1,25 @@
 package core.basesyntax;
 
+import java.util.LinkedList;
+
 public class MyHashMap<K, V> implements MyMap<K, V> {
+    private static class Entry<K, V> {
+        K key;
+        V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    LinkedList<Entry<K, V>>[] table;
+    int initialCapacity = 16;
+    double loadFactor = 0.75;
 
     @Override
     public void put(K key, V value) {
-
+        table[getIndex(key)].add((Entry<K, V>) value);
     }
 
     @Override
@@ -15,5 +30,22 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
     @Override
     public int getSize() {
         return 0;
+    }
+
+    public void resize(int initialCapacity) {
+        while (initialCapacity > table.length) {
+            int newCapacity = (int)(table.length * loadFactor);
+            LinkedList<Entry<K, V>>[] newTable;
+            System.arraycopy(array, 0, newArray, 0, size);
+            initialCapacity = newCapacity;
+        }
+    }
+
+    public int getIndex(K key) {
+        int position = 0;
+        if (key != null) {
+            position = key.hashCode() % table.length;
+        }
+        return position;
     }
 }
