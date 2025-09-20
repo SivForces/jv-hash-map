75b2aec4074a52926afaa4370d0f3d2663843599
Vitalii Levda <vitalii.levda@tul.cz>
2025-09-20 16:05:48 +0300

    fields changed (constants added), threshold added, put() edited, getValue(), getSize(), resize() added. Other minor changes to functionality

--- src/main/java/core/basesyntax/MyHashMap.java
+++ src/main/java/core/basesyntax/MyHashMap.java
@@ -13,39 +13,84 @@ public class MyHashMap<K, V> implements MyMap<K, V> {
         }
     }
 
-    LinkedList<Entry<K, V>>[] table;
-    int initialCapacity = 16;
-    double loadFactor = 0.75;
+    final static int INITIAL_CAPACITY = 16;
+    final static double LOAD_FACTOR = 0.75;
+    private LinkedList<Entry<K, V>>[] table = new LinkedList[INITIAL_CAPACITY];
+    private int size = 0;
+    double threshold = INITIAL_CAPACITY * LOAD_FACTOR;
 
     @Override
-    public void put(K key, V value) {
-        table[getIndex(key)].add((Entry<K, V>) value);
+    public V put(K key, V value) {
+        if (key == null) {
+            if (table[0] == null) {
+                table[0] = new LinkedList<>();
+            }
+
+            table[0].add(new Entry<>(key, value));
+        }
+        int index = getIndex(key);
+        if (table[index] != null) {
+            for (var entry : table[index]) {
+                if (entry.key.hashCode() == key.hashCode()) {
+                    var oldValue = entry.value;
+                    entry.value = value;
+                    return oldValue;
+                }
+            }
+        } else {
+            table[index] = new LinkedList<>();
+        }
+        table[index].add(new Entry<>(key, value));
+        size++;
+        resize(size);
+        return null;
     }
 
     @Override
     public V getValue(K key) {
+        for (LinkedList<Entry<K, V>> entries : table) {
+            if (entries != null) {
+                for (var entry : entries) {
+                    if (entry.key == key) {
+                        return entry.value;
+                    }
+                }
+            }
+        }
         return null;
     }
 
     @Override
     public int getSize() {
-        return 0;
+        return size;
     }
 
-    public void resize(int initialCapacity) {
-        while (initialCapacity > table.length) {
-            int newCapacity = (int)(table.length * loadFactor);
-            LinkedList<Entry<K, V>>[] newTable;
-            System.arraycopy(array, 0, newArray, 0, size);
-            initialCapacity = newCapacity;
+    private void resize(int size) {
+        if (size > threshold) {
+            int newCapacity = table.length * 2;
+            LinkedList<Entry<K, V>>[] newTable = new LinkedList[newCapacity];
+            int index;
+            for (LinkedList<Entry<K, V>> entries : table) {
+                if (entries != null) {
+                    for (var entry : entries) {
+                        index = (entry.key.hashCode() & 0x7fffffff) % newCapacity;
+                        if (newTable[index] == null) {
+                            newTable[index] = new LinkedList<>();
+                        }
+                        newTable[index].add(entry);
+                    }
+                }
+            }
+            table = newTable;
+            threshold = (int)(newCapacity * LOAD_FACTOR);
         }
     }
 
     public int getIndex(K key) {
-        int position = 0;
+        int index = 0;
         if (key != null) {
-            position = key.hashCode() % table.length;
+            index = (key.hashCode() & 0x7fffffff) % table.length;
         }
-        return position;
+        return index;
     }
 }
